12f68467a18d10b0a8c2cc90dc4cf3168d6a4c13
Srilakshmi Ranganath <srilakshmiranganath8@gmail.com>
2024-08-29 18:14:37 +0530

    model evaluation completed

---
+++ src/textSummarizer/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,12 @@
+from textSummarizer.components.data_transformation import DataTransformation
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.logging import logger
+
+class DataTransformationTrainingPipeline:
+    def __init__(self) -> None:
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.convert()
